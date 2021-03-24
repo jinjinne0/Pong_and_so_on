4b2c3a23e3177a1316408b02a5949d23737b3396
zer0x <juicemakimono@gmail.com>
2021-03-25 05:28:22 +0900

    write rough flow of game by comments

--- Shooting/Game.cpp
+++ Shooting/Game.cpp
@@ -1,5 +1,4 @@
 #include "Game.hpp"
-#include "SDL/SDL.h"
 
 //--------------------------------
 //Macro definition(not revealed)
@@ -14,29 +13,155 @@
 //--------------------------------
 //global variable definition
 //--------------------------------
-
+const float kShooterSpeed = 400.0f; //pixels per seconds
+const int kWindowWidth = 1024;
+const int kWindowHeight = 768;
+const float kEps = 1e-9; // 
 
 //--------------------------------
 //Function definition
 //--------------------------------
 
-Game::Game()
+Vector2::Vector2(float x = 0.0f, float y = 0.0f):x(x),y(y){}
+
+Color::Color(short int R = 0, short int G = 0, short int B = 0, short int A =0)
+:R(R), G(G), B(B), A(A)
 {}
 
-bool Game::Initialize(){}
+//TODO: 簡単な変数の初期化
+Game::Game()
+:mIsRunning(true)
+,mIsPaused(true)
+,mTicksCount(0)
+,mPausingTicks(0)
+,mWindow(nullptr)
+,mRenderer(nullptr)
+{}
+
+//If initialization success, return true. If not, return false
+bool Game::Initialize(){
+
+    //Initialize SDL
+    int sdl_init = SDL_Init(SDL_INIT_VIDEO);
+    if(sdl_init != 0){
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return false;
+    }
+
+    //Create SDL Window
+    mWindow = SDL_CreateWindow(
+        "Shooting",  //title for window
+        100,        //top left x-coordinate
+        100,        //top left y-coordinate
+        kWindowWidth,       //width
+        kWindowHeight,        //height
+        0  //0 for default
+    );
+    if(!mWindow){
+        SDL_Log("Failed to create SDL window: %s", SDL_GetError());
+        return false;
+    }
+
+    //Create SDL Renderer for 2D drawing
+    mRenderer = SDL_CreateRenderer(
+        mWindow,  //SDL Window to draw
+        -1,       //-1 for normal
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+    );
+    if(!mRenderer){
+        SDL_Log("Failed to create SDL Renderer: %s", SDL_GetError());
+        return false;
+    }
+
+    //TODO:Initialize shooter
+    mShooter.coordinate = Vector2();
+    mShooter.width;
+    mShooter.vell;
+    mShooter.color = Color();
+
+    //TODO: Prepare for game loop
+
+    //success to initialize the game
+    return true;
+}
 
 void Game::RunLoop(){
+
     while(mIsRunning){
         ProcessInput();
         UpdateGame();
         GenerateOutput();
     }
+
+    //display your point
+    printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+    printf("       Your point is %dpt\n", (mTicksCount - mPausingTicks)/1000);
+    printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
 }
 
-void Game::Shutdown(){}
+void Game::Shutdown(){
+    SDL_DestroyWindow(mWindow);
+    SDL_DestroyRenderer(mRenderer);
+    SDL_Quit();
 
-void Game::ProcessInput(){}
+    //TODO:Initialize()で初期化などしたものを排除
+    //動的に確保したメモリも削除
+}
 
-void Game::UpdateGame(){}
+void Game::ProcessInput(){
 
-void Game::GenerateOutput(){}
+    //If there QUIT_event, end game
+    SDL_Event event;
+    while(SDL_PollEvent(&event)){
+        if(event.type = SDL_QUIT){
+            mIsRunning = false;
+        }
+    }
+
+    //get the state of keyboard
+    const Uint8* state = SDL_GetKeyboardState(NULL);
+
+    // Escape key get the game end
+    if(state[SDL_SCANCODE_ESCAPE]){
+        mIsRunning = false;
+    }
+
+    //Setting S-key to pause、A-key to restart the game
+    if(state[SDL_SCANCODE_A]){
+        mIsPaused = false;
+    }
+    if(state[SDL_SCANCODE_S]){
+        mIsPaused = true;
+    }
+
+    //E-key : slide shooter up 
+    //D-key : slide shooter down
+    short int shooter_direction = 0;
+    if(state[SDL_SCANCODE_E]){
+        shooter_direction--;
+    }
+    if(state[SDL_SCANCODE_D]){
+        shooter_direction++;
+    }
+    mShooter.vell.y = shooter_direction * kShooterSpeed;
+
+    //TODO: Kでbullet発射
+
+}
+
+void Game::UpdateGame(){
+    //TODO: deltatimeの取得
+    //TODO: mTicksCountの更新
+    //TODO: Pauseしてるなら終了.PausingTicksの更新
+    //TODO: Shooterの更新
+    //TODO: Enemyの更新(mIsPausingの更新)
+    //TODO: Bulletの更新(Enemyの削除も)
+}
+
+void Game::GenerateOutput(){
+    //TODO: 壁
+    //TODO: Shooter
+    //TODO: Enemy
+    //TODO: Bullets
+    //バッファの取り替え
+}
--- Shooting/Game.hpp
+++ Shooting/Game.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "SDL/SDL.h"
+#include "stdio.h"
+#include <list>
 
 //--------------------------------
 //Macro definition
@@ -11,6 +13,30 @@
 //Type definition
 //--------------------------------
 
+struct Vector2{
+    Vector2(float, float);
+    float x, y;
+};
+
+typedef Vector2 Point2;
+
+struct Color{
+    Color(short int, short int, short int, short int);
+    short int R, G, B, A;
+};
+
+//class for dynamic object
+class PongObject{
+public:
+    //Coordinatee of the centeer of object
+    Point2 coordinate;
+    float width, height;
+    //vellocity
+    Vector2 vell;
+    Color color;
+};
+
+//Class that controlls the game
 class Game{
 public:
     Game::Game();
@@ -24,8 +50,26 @@ private:
     void UpdateGame();
     void GenerateOutput();
 
+    //SDL window
+    SDL_Window* mWindow;
+    //SDL Renderer for 2D drawing
+    SDL_Renderer* mRenderer;
     //Should we continue the game?
     bool mIsRunning;
+    //now Paused?
+    bool mIsPaused;
+    //Ticks Count
+    Uint32 mTicksCount;
+    //ticks while paused
+    Uint32 mPausingTicks;
+
+    //specific for Shooting
+    PongObject mShooter;
+    //List of bullets
+    std::list<PongObject> mBullets;
+    //List of enemy
+    std::list<PongObject> mEnemies;
+
 
 };
 

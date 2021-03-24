044de62804b4564e04a274d894e26c7b13a9ceb5
zer0x <juicemakimono@gmail.com>
2021-03-24 15:15:40 +0900

    set important Prototype of memebers of Game

--- Shooting/Game.hpp
+++ Shooting/Game.hpp
@@ -11,6 +11,23 @@
 //Type definition
 //--------------------------------
 
+class Game{
+public:
+    Game::Game();
+    bool Initialize();
+    void RunLoop();
+    void Shutdown();
+
+private:
+    //Helper functions for RunLoop
+    void ProcessInput();
+    void UpdateGame();
+    void GenerateOutput();
+
+    //Should we continue the game?
+    bool mIsRunning;
+
+};
 
 //--------------------------------
 //Prototype definition

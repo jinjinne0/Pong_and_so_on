8abae838eb8511305b6e4b72decbfb87a437ba92
zer0x <juicemakimono@gmail.com>
2021-03-24 15:07:19 +0900

    write rough game flow in Main.cpp

--- Shooting/Main.cpp
+++ Shooting/Main.cpp
@@ -3,6 +3,12 @@
 #include "SDL/SDL.h"
 
 int main(int argv, char** argc){
+    Game game;
+    bool init_result = game.Initialize();
+    if(init_result){
+        game.RunLoop();
+    }
+    game.Shutdown();
 
     return 0;
 }
